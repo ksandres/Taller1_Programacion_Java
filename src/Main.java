1bbe97ae0dfd6d7446f9bce27f6417184d97e812
andres <anfega3@gmail.com>
2023-08-23 03:44:18 -0500

    actividad 1 tenis

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese un numero");
+        System.out.println("Ingrese los sets de A:");
         int a = scanner.nextInt();
-        System.out.println("Ingrese un numero:");
+        System.out.println("Ingrese los sets de B:");
         int b = scanner.nextInt();
 
-        float division = (float) a/b;
-        System.out.println("la divicion es " + division);
-        float modulo = a%b;
-        if (modulo == 0){System.out.println("la divion es exacta");
-        }else {System.out.println("La division no es exacta");}
+        if (a < 0 || b < 0 || a >= 8 || b >= 8) {
+            System.out.println("Los valores ingresados son inválidos.");
+        } else if (a == 6 && b < 5) {
+            System.out.println("El set es de A.");
+        } else if (b == 6 && a < 5) {
+            System.out.println("El set es de B.");
+        } else if ((a == 7 && b >= 5) || (b == 7 && a >= 5)) {
+            if (a == 7) {
+                System.out.println("El set es de A");
+            } else {
+                System.out.println("El set es de B");
+            }
+        }else if (a == 7 || b == 7) {
+                System.out.println("El set no ha terminado.");
 
 
-
+            }
         }
     }
